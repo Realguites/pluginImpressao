faed0f55fba3eec2a8e0abeed7dd43d0c4a0f000
CGM - Guilherme <guilherme.tes@gmail.com>
2022-05-12 15:34:12 -0300

    Tratando exceptions para impressora nao encontrada ou invalida

--- server2.py
+++ server2.py
@@ -1,10 +1,14 @@
-import os
+from http.client import INSUFFICIENT_STORAGE, HTTPException
+import io
+import flask
 import win32print
 import json
-import win32api
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS, cross_origin
-
+from base64 import b64decode
+from glob import glob
+import win32ui
+from PIL import Image, ImageWin
 UPLOAD_FOLDER = './uploads'
 
 app = Flask(__name__)
@@ -12,33 +16,63 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/imprimirteste', methods=['GET'])
-#@cross_origin()
-def imprimir():
-    lista_impressoras = win32print.EnumPrinters(2)
-    print(lista_impressoras[1])
+@app.route('/getAllPrinters', methods=['GET'])
+@cross_origin()
+def getAllPrinters():
+    lista_impressoras = [printer[2] for printer in win32print.EnumPrinters(2)]
     return json.dumps(lista_impressoras)
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/imprimirteste', methods=['GET','POST'])
 @cross_origin()
-def upload_file():
+def receiveFile():
     if request.method == 'POST':
-        if 'file1' not in request.files:
-            return 'there is no file1 in form!'
-        file1 = request.files['file1']
-        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
-        file1.save(path)
-        return path
+        if not request.form.get('printer'):
+            return Response("Impressora não informada!", status="400", mimetype='application/json')
+        else:
+            if request.form.get('printer') not in getPrinters():
+                return Response("Impressora não encontrada!", status="400", mimetype='application/json')
+            else:
+                pdf_64 = request.form.get("pdf_64")
+                bytes = b64decode(pdf_64, validate=True)
+                callPrinterService('Microsoft Print to PDF', bytes)
 
-        return 'ok'
-    return '''
-    <h1>Upload new File</h1>
-    <form method="post" enctype="multipart/form-data">
-      <input type="file" name="file1">
-      <input type="submit">
-    </form>
-    '''
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+def getPrinters():
+    return [printer[2] for printer in win32print.EnumPrinters(2)]
+
+def callPrinterService(printer, bytes):
+    file_name = "teste.png"
+    hDC = win32ui.CreateDC ()
+    hDC.CreatePrinterDC (printer)
+    buf = io.BytesIO(bytes)
+    bmp = Image.open (buf)
+    hDC.StartDoc (file_name)
+    hDC.StartPage ()
+    dib = ImageWin.Dib (bmp)
+   
+    x1 = 50
+    y1 = 50
+    x2 = 2000 + bmp.size[0]
+    y2 = 3000 + bmp.size[1]
+    dib.draw (hDC.GetHandleOutput (), (x1, y1, x2, y2))
+
+    hDC.EndPage ()
+    hDC.EndDoc ()
+    hDC.DeleteDC ()
 
 if __name__ == '__main__':
     app.run()
